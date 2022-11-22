addad8c93fdf515720b40d5aef3db13aecefdd17
xMathyu <xmathyu@gmail.com>
2022-11-21 19:41:29 -0500

    connect_marker added

--- App.py
+++ App.py
@@ -12,7 +12,17 @@ class App(tkinter.Tk):
     WIDTH = 800
     HEIGHT = 800
 
+    def connect_marker(self):
+        position_list = []
 
+        for marker in self.marker_list:
+            position_list.append(marker.position)
+
+        if self.marker_path is not None:
+            self.map_widget.delete(self.marker_path)
+
+        if len(position_list) > 0:
+            self.marker_path = self.map_widget.set_path(position_list)
 
     if __name__ == "__main__":
     app = App()
